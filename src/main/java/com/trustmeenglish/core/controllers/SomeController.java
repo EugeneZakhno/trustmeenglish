e83d0af4e75d159531a8cc1dd9f2369bdf5003c5
EugeneZakhno <zohan1234567@gmail.com>
2020-05-15 23:42:43 +0300

    added SomeController

--- src/main/java/com/trustmeenglish/core/controllers/SomeController.java
+++ src/main/java/com/trustmeenglish/core/controllers/SomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SomeController {
 
-    @GetMapping("/hellow")
+    @GetMapping("/hello")
     public String sayHello(){
         return "hello world";
     }
